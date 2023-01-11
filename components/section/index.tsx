eabceba70d60f74cf9d38eeb70e15c21001ea75c
Mohammad Moid <moid.mw@gmail.com>
2023-01-11 19:03:25 +0530

    Revert "develop-pull-1 (#2)"
    This reverts commit 01a66a19e3df1d53c667432b8d4b98dd3340a15b.

--- components/section/index.tsx
+++ components/section/index.tsx
@@ -2,12 +2,11 @@ import React from "react";
 
 type Props = {
   children?: React.ReactNode;
-  className?: string;
 };
 
-const Section = ({ children, className }: Props) => {
+const Section = ({ children }: Props) => {
   return (
-    <div className={`flex flex-col flex-wrap text-center h-screen content-center justify-center bg-gray-300 border-b-2 border-slate-600 px-32 ${className}`}>
+    <div className="flex flex-col flex-wrap text-center h-screen content-center justify-center bg-gray-300 border-b-2 border-slate-600 px-32">
       {children}
     </div>
   );
